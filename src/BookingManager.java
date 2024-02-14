ebe8cde2e34a02c9e42f5d10801e7353dff88633
MichalKlemm <Klemmmichal@lgmail.com>
2024-02-14 19:51:39 +0100

    přidání metod do Bookingmanageru
    výpis všech rezervací
    výpis prvních 8 rezervací
    statistika podle počtu hostů

--- src/BookingManager.java
+++ src/BookingManager.java
@@ -44,6 +44,46 @@ public class BookingManager {
         return recreationalBookings;
     }
 
+    public void printAllBookings(){
+        for (Booking booking : bookings){
+            System.out.println(booking);;
+        }
+    }
+
+    public void printFirstRecreationalBookings(int n){
+        int count = 0;
+        for (Booking booking : bookings){
+            if (!booking.getTypeOfVacation()){
+                System.out.println(booking);
+                count++;
+            }
+            if (count == n){
+                break;
+            }
+        }
+    }
+
+    public void printGuestStatistics(){
+        int singleGuest = 0;
+        int doubleGuest = 0;
+        int multipleGuest = 0;
+
+        for (Booking booking : bookings){
+            int guestCount = booking.getGuests().size();
+            if (guestCount == 1) {
+                singleGuest++;
+            }else if (guestCount == 2){
+                    doubleGuest++;
+                    }else {
+                        multipleGuest++;
+                    }
+                }
+                System.out.println("Celkový počet rezervací s jedním hostem: " + singleGuest);
+                System.out.println("Celkový počet rezervací se dvěma hosty: " + doubleGuest);
+                System.out.println("Celkový počet rezervací s více než dvěma hosty: " + multipleGuest);
+            }
+        }
+
     public double getAverageGuests(){
         if (bookings.isEmpty()){
             return 0.0;
