2690d5330e5aba31af5f635618260e4bb9c954b8
MichalKlemm <Klemmmichal@lgmail.com>
2024-02-14 18:31:50 +0100

    Přidání metod
    přidána metoda pro výpis pracovních a rekreačních pobytů + metoda pro výpis průměrného počtu hostů.

--- src/BookingManager.java
+++ src/BookingManager.java
@@ -5,20 +5,55 @@ import java.util.List;
 
 public class BookingManager {
 
-    private int getNumberOfWorkingBookings;
+    private final List<Booking> bookings; //= new ArrayList<>();
 
-    private final List<Booking> bookings = new ArrayList<>();
+    public BookingManager(List<Booking> bookings){
+        this.bookings = new ArrayList<>(bookings);
+    }
 
-    public void addBooking(Booking booking){
-        bookings.add(booking);
+    public void addBooking(Booking newBooking){
+        bookings.add(newBooking);
     }
     public void removeBooking(Booking booking){
         bookings.remove(booking);
     }
-    public void clearBookings(Booking booking){
+    public void clearBookings(){
         bookings.clear();
     }
     public List<Booking> getBookings(){
         return new ArrayList<>(bookings);
     }
+
+    public int getNumberOfWorkingBookings(){
+        int workingBookings = 0;
+        for (Booking booking : bookings){
+            if (booking.getTypeOfVacation()){
+                workingBookings++;
+            }
+        }
+        return workingBookings;
+    }
+
+    public int getNumberOfRecreationalBookings(){
+        int recreationalBookings = 0;
+        for (Booking booking : bookings){
+        if (!booking.getTypeOfVacation()) {
+            recreationalBookings++;
+            }
+        }
+        return recreationalBookings;
+    }
+
+    public double getAverageGuests(){
+        if (bookings.isEmpty()){
+            return 0.0;
+        }
+
+        int totalGuests = 0;
+        for (Booking booking : bookings){
+            totalGuests += booking.getNumberOfGuests();
+        }
+        return (double) totalGuests / bookings.size();
+    }
+
 }
